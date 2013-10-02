bb1dca8f4dd984d0748b9e3fe8e64b417131bc79
Cenk Alti <cenkalti@gmail.com>
2013-10-02 13:37:27 +0300

    connect to mysql

--- dalga.go
+++ dalga.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"code.google.com/p/gcfg"
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"net/http"
 )
 
-type Config struct {
+var cfg struct {
 	MySQL struct {
-		Uri   string
-		Table string
+		Driver string
+		Dsn    string
+		Table  string
 	}
 	RabbitMQ struct {
 		Uri      string
@@ -21,18 +25,37 @@ type Config struct {
 	}
 }
 
+var db *sql.DB
+
 func handleSchedule(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("schedule")
 }
 
+func handleCancel(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("cancel")
+}
+
 func main() {
-	var cfg Config
+	// Read config
 	err := gcfg.ReadFileInto(&cfg, "dalga.ini")
 	if err != nil {
-		fmt.Errorf("Cannot read config file")
+		log.Fatal(err)
 	}
+	fmt.Println("Read config: ", cfg)
 
+	// Connect to MySQL
+	_, err = sql.Open(cfg.MySQL.Driver, cfg.MySQL.Dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MySQL")
+
+	// Connect to RabbitMQ
+	// fmt.Println("Connected to MySQL")
+
+	// Start HTTP server
 	addr := cfg.HTTP.Host + ":" + cfg.HTTP.Port
 	http.HandleFunc("/schedule", handleSchedule)
+	http.HandleFunc("/cancel", handleSchedule)
 	http.ListenAndServe(addr, nil)
 }
