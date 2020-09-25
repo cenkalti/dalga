a50416f02d6e8f90688b73f54a71448d79254033
Matt Mc <4984708+tooolbox@users.noreply.github.com>
2020-09-24 17:29:42 -0700

    Add succession test.

--- cluster_test.go
+++ cluster_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -117,6 +118,124 @@ func TestCluster(t *testing.T) {
 
 		if len(countsByInstance) != len(instances) {
 			t.Fatalf("Expected each instance to have done some jobs.")
+		}
+
+		time.Sleep(time.Second)
+	})
+}
+
+func TestClusterSuccession(t *testing.T) {
+	numInstances := 3
+	instances := make([]*Dalga, numInstances)
+	ctxes := make([]context.Context, numInstances)
+	cancels := make([]func(), numInstances)
+	for i := 0; i < numInstances; i++ {
+		ctxes[i], cancels[i] = context.WithCancel(context.Background())
+	}
+	killStart, killDone := make(chan string), make(chan string)
+	kill := func(w http.ResponseWriter, r *http.Request) {
+		instance := r.Header.Get("dalga-instance")
+		killStart <- instance
+		killDone <- instance
+		w.WriteHeader(200)
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/kill", kill)
+	srv := httptest.NewServer(mux)
+	defer srv.Close()
+
+	var client *Client
+	for i := 0; i < numInstances; i++ {
+		config := DefaultConfig
+		config.MySQL.SkipLocked = false
+		config.MySQL.Table = "test_cluster_succession"
+		config.Jobs.FixedIntervals = true
+		config.Jobs.ScanFrequency = 100 * time.Millisecond
+		config.Endpoint.BaseURL = "http://" + srv.Listener.Addr().String() + "/"
+		config.Listen.Port = 34100 + i
+
+		d, lis, cleanup := newDalga(t, config)
+		instances[i] = d
+		if i == 0 {
+			client = NewClient("http://" + lis.Addr())
+		}
+		defer cleanup()
+	}
+
+	for i, inst := range instances {
+		go inst.Run(ctxes[i])
+	}
+	defer func() {
+		for i, inst := range instances {
+			if fn := cancels[i]; fn != nil {
+				fn()
+			}
+			<-inst.NotifyDone()
+		}
+	}()
+
+	t.Run("reclaim jobs", func(t *testing.T) {
+		start := time.Now().Add(time.Second)
+		_, err := client.Schedule(context.Background(), "kill", "bill", WithFirstRun(start), WithOneOff())
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// An instance is going to run the job.
+		var doomedInstance string
+		select {
+		case doomedInstance = <-killStart:
+		case <-time.After(time.Second * 2):
+			t.Fatal("Didn't get the kill start")
+		}
+
+		// Now we kill it before the cycle of action completes.
+		t.Logf("Killing instance %s", doomedInstance)
+		id, err := strconv.ParseUint(doomedInstance, 10, 32)
+		if err != nil {
+			t.Fatal(err)
+		}
+		var done chan struct{}
+		for i, inst := range instances {
+			if inst.instance.ID() == uint32(id) {
+				done = inst.NotifyDone()
+				cancels[i]()
+				cancels[i] = nil
+				break
+			}
+		}
+		if done == nil {
+			t.Fatalf("Couldn't locate instance with id %d", id)
+		}
+		select {
+		case <-done:
+		case <-time.After(time.Second * 2):
+			t.Fatal("Didn't get the done signal from the dead Dalga")
+		}
+		select {
+		case <-killDone:
+		case <-time.After(time.Second * 2):
+			t.Fatal("Didn't finish the kill")
+		}
+
+		// Now, another instance will pick up that job and try again.
+		var replacement string
+		select {
+		case replacement = <-killStart:
+		case <-time.After(time.Second * 2):
+			t.Fatal("Job wasn't picked up by a replacement instance")
+		}
+		if replacement == doomedInstance {
+			t.Fatal("Something is very wrong")
+		}
+		select {
+		case finished := <-killDone:
+			if finished != replacement {
+				t.Fatalf("Something else is very wrong, finished is '%s' but should be '%s'", finished, replacement)
+			}
+		case <-time.After(time.Second * 2):
+			t.Fatal("Job was never finished")
 		}
 
 		time.Sleep(time.Second)
